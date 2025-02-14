6312cf2b7f78e774e45a17b40d976e782e7bdc5d
msk226 <kminseok5167@gmail.com>
2025-02-14 13:25:55 +0900

    [FEAT] 초기 파일 업로드

---
+++ src/test/java/hyu/erica/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package hyu.erica.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
