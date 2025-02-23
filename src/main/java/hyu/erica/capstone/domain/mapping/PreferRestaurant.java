09969f4b26cf2aa8e7ad26504c86b412c5a57754
msk226 <kminseok5167@gmail.com>
2025-02-23 15:10:45 +0900

    [FEATURE] 선호 여행지, 선호 식당 엔티티 생성

---
+++ src/main/java/hyu/erica/capstone/domain/mapping/PreferRestaurant.java
@@ -0,0 +1,42 @@
+package hyu.erica.capstone.domain.mapping;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import hyu.erica.capstone.domain.Restaurant;
+import hyu.erica.capstone.domain.User;
+import hyu.erica.capstone.domain.base.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PreferRestaurant extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+//    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean isPrefer;
+}
