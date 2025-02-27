569547b92bed385f5394405e88a1dd86b6eb6111
msk226 <kminseok5167@gmail.com>
2025-02-27 17:56:19 +0900

    [FEATURE] 선택지 관련 API 시그니처 작성

--- src/main/java/hyu/erica/capstone/web/controller/TripPlanController.java
+++ src/main/java/hyu/erica/capstone/web/controller/TripPlanController.java
@@ -3,10 +3,13 @@ package hyu.erica.capstone.web.controller;
 import hyu.erica.capstone.api.ApiResponse;
 import hyu.erica.capstone.web.dto.trip.request.AdditionalInfoRequestDTO;
 import hyu.erica.capstone.web.dto.trip.request.PreferActivitiesRequestDTO;
+import hyu.erica.capstone.web.dto.trip.request.SaveAttractionRequestDTO;
+import hyu.erica.capstone.web.dto.trip.request.SaveRestaurantRequestDTO;
 import hyu.erica.capstone.web.dto.trip.request.TripPeriodRequestDTO;
 import hyu.erica.capstone.web.dto.trip.response.AdditionalInfoResponseDTO;
 import hyu.erica.capstone.web.dto.trip.response.PreferActivitiesResponseDTO;
 import hyu.erica.capstone.web.dto.trip.response.TripPeriodResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +20,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Trip", description = "여행 관련 API")
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api/trip-plans/{tripPlansId}")
 public class TripPlanController {
 
     // 여행 기간 입력
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "여행 기간 입력", description = """
+            ### 여행 기간을 입력합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            
+            ### Request Body
+            - startDate: 여행 시작 날짜
+            - startHour: 여행 시작 시간
+            - endDate: 여행 종료 날짜
+            - endHour: 여행 종료 시간
+                  """)
     @PostMapping("/period")
     public ApiResponse<?> inputPeriod(
             @PathVariable Long tripPlansId,
@@ -32,6 +48,13 @@ public class TripPlanController {
         return null;
     }
 
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "여행 기간 조회", description = """
+            ### 여행 기간을 조회합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     @GetMapping("/period")
     public ApiResponse<TripPeriodResponseDTO> getPeriod(
             @PathVariable Long tripPlansId) {
@@ -40,6 +63,16 @@ public class TripPlanController {
     }
 
     // 선호 활동 입력
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "선호 활동 입력", description = """
+            ### 선호 활동을 입력합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            
+            ### Request Body
+            - activities: 선호 활동 (String)
+                  """)
     @PostMapping("/activities")
     public ApiResponse<?> inputActivities(
             @PathVariable Long tripPlansId,
@@ -48,6 +81,13 @@ public class TripPlanController {
         return null;
     }
 
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "선호 활동 조회", description = """
+            ### 선호 활동을 조회합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     @GetMapping("/activities")
     public ApiResponse<PreferActivitiesResponseDTO> getActivities(
             @PathVariable Long tripPlansId) {
@@ -56,6 +96,16 @@ public class TripPlanController {
     }
 
     // 추가적인 요구 사항 입력
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "추가적인 요구 사항 입력", description = """
+            ### 추가적인 요구 사항을 입력합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            
+            ### Request Body
+            - additionalInfo: 추가적인 요구 사항 (String)
+                  """)
     @PostMapping("/additional")
     public ApiResponse<?> inputAdditional(
             @PathVariable Long tripPlansId,
@@ -64,6 +114,13 @@ public class TripPlanController {
         return null;
     }
 
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "추가적인 요구 사항 조회", description = """
+            ### 추가적인 요구 사항을 조회합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     @GetMapping("/additional")
     public ApiResponse<AdditionalInfoResponseDTO> getAdditional(
             @PathVariable Long tripPlansId) {
@@ -71,6 +128,13 @@ public class TripPlanController {
         return null;
     }
 
+    @Tag(name = "Trip", description = "여행 관련 API")
+    @Operation(summary = "일정 제공", description = """
+            ### 최종적으로 입력한 정보를 바탕으로 일정을 제공합니다.
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     // 최종 입력 ( AI API 호출)
     @GetMapping("/final")
     public ApiResponse<?> finalInput(
@@ -79,7 +143,16 @@ public class TripPlanController {
         return null;
     }
 
+    /* ------------------------------------------------------------------------------------------------------ */
+
     // 선택지 확인 (여행지)
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "선택지 (여행지) 확인", description = """
+            ### 선택지 확인 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     @GetMapping("/attractions")
     public ApiResponse<?> checkPlaces(
             @PathVariable Long tripPlansId) {
@@ -88,6 +161,14 @@ public class TripPlanController {
     }
 
     // 선택지 상세 보기 (여행지)
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "선택지 (여행지) 상세 보기", description = """
+            ### 선택지 상세 보기 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            - attractionId: 여행지 ID
+                  """)
     @GetMapping("/attractions/{attractionId}")
     public ApiResponse<?> viewPlaceDetail(
             @PathVariable Long tripPlansId,
@@ -97,6 +178,16 @@ public class TripPlanController {
     }
 
     // 여행지 키워드 검색
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "여행지 키워드 검색", description = """
+            ### 여행지 키워드 검색 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            
+            ### Request Param
+            - keyword: 검색 키워드
+                  """)
     @GetMapping("/attractions/search")
     public ApiResponse<?> searchPlace(
             @PathVariable Long tripPlansId,
@@ -106,14 +197,30 @@ public class TripPlanController {
     }
 
     // 여행지 최종 선택
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "여행지 최종 선택", description = """
+            ### 여행지 최종 선택 API
+            
+            ### Path Variables
+            - tripPlansIds: 여행 계획 ID (List)
+            
+                  """)
     @PostMapping("/attractions/final")
     public ApiResponse<?> finalPlace(
-            @PathVariable Long tripPlansId) {
+            @PathVariable Long tripPlansId,
+            @RequestBody SaveAttractionRequestDTO request) {
         // 여행지 최종 선택
         return null;
     }
 
     // 선택지 확인 (음식점)
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "선택지 (음식점) 확인", description = """
+            ### 선택지 확인 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+                  """)
     @GetMapping("/restaurants")
     public ApiResponse<?> checkRestaurants(
             @PathVariable Long tripPlansId) {
@@ -123,6 +230,14 @@ public class TripPlanController {
 
 
     // 선택지 상세 보기 (음식점)
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "선택지 (음식점) 상세 보기", description = """
+            ### 선택지 상세 보기 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            - restaurantId: 음식점 ID
+                  """)
     @GetMapping("/restaurants/{restaurantId}")
     public ApiResponse<?> viewRestaurantDetail(
             @PathVariable Long tripPlansId,
@@ -133,6 +248,16 @@ public class TripPlanController {
 
 
     // 음식점 키워드 검색
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "음식점 키워드 검색", description = """
+            ### 음식점 키워드 검색 API
+            
+            ### Path Variables
+            - tripPlansId: 여행 계획 ID
+            
+            ### Request Param
+            - keyword: 검색 키워드
+                  """)
     @GetMapping("/restaurants/search")
     public ApiResponse<?> searchRestaurant(
             @PathVariable Long tripPlansId,
@@ -143,9 +268,17 @@ public class TripPlanController {
 
 
     // 음식점 최종 선택
+    @Tag(name = "선택지 확인", description = "선택지 확인 API")
+    @Operation(summary = "음식점 최종 선택", description = """
+            ### 음식점 최종 선택 API
+            
+            ### Path Variables
+            - tripPlansIds: 여행 계획 ID (List)
+                  """)
     @PostMapping("/restaurants/final")
     public ApiResponse<?> finalRestaurant(
-            @PathVariable Long tripPlansId) {
+            @PathVariable Long tripPlansId,
+            @RequestBody SaveRestaurantRequestDTO request) {
         // 음식점 최종 선택
         return null;
     }
---
+++ src/main/java/hyu/erica/capstone/web/dto/trip/request/SaveRestaurantRequestDTO.java
@@ -0,0 +1,6 @@
+package hyu.erica.capstone.web.dto.trip.request;
+
+import java.util.List;
+
+public record SaveRestaurantRequestDTO (List<Long> restaurantIds) {
+}
