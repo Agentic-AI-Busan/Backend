b417a9facde79e1ec8c3f7c7bac8e6046f154f89
msk226 <kminseok5167@gmail.com>
2025-02-27 17:00:01 +0900

    [FEATURE] Health Check API 명세서 작성

--- src/main/java/hyu/erica/capstone/controller/DefaultController.java
+++ src/main/java/hyu/erica/capstone/controller/DefaultController.java
@@ -1,11 +1,13 @@
 package hyu.erica.capstone.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
 
+    @Operation(summary = "Health Check", description = "서버 상태 확인")
     @GetMapping("/health-check")
     public String healthCheck() {
         return "Hello, World!";
