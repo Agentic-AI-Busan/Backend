5f45bfab522b8ab28b2c51b33b99f5c0eda665f9
msk226 <kminseok5167@gmail.com>
2025-04-11 11:31:20 +0900

    [FEATURE - V2]: 테스트 코드에 부트스트랩 클래스 명시

--- src/test/java/hyu/erica/capstone/CapstoneApplicationTests.java
+++ src/test/java/hyu/erica/capstone/CapstoneApplicationTests.java
@@ -1,9 +1,10 @@
 package hyu.erica.capstone;
 
+import hyu.erica.v1.capstone.CapstoneApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CapstoneApplication.class)
 class CapstoneApplicationTests {
 
 	@Test
