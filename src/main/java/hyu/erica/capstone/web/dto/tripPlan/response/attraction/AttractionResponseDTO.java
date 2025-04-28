12e63ea3210e8610e35b31442d12f61f3ced04b1
msk226 <kminseok5167@gmail.com>
2025-04-28 19:06:47 +0900

    [FEATURE] 추천 여행지 조회 시 프론트 화면 구성용 상세 필드 추가

--- src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionResponseDTO.java
+++ src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionResponseDTO.java
@@ -1,8 +1,11 @@
 package hyu.erica.capstone.web.dto.tripPlan.response.attraction;
 
-public record AttractionResponseDTO(Long attractionId, String name, String imageUrl, String address) {
+public record AttractionResponseDTO(Long attractionId, String name, String imageUrl, String address,
+                                    String operatingHours, String title, Double latitude, Double longitude) {
 
-    public static AttractionResponseDTO of(Long attractionId, String name, String imageUrl, String address) {
-        return new AttractionResponseDTO(attractionId, name, imageUrl, address);
+    public static AttractionResponseDTO of(Long attractionId, String name, String imageUrl, String address,
+            String operatingHours, String title, Double latitude, Double longitude) {
+        return new AttractionResponseDTO(attractionId, name, imageUrl, address,
+                operatingHours, title, latitude, longitude);
     }
 }
