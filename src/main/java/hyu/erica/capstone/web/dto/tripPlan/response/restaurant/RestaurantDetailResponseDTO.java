e1e912df0981fccaa75e30015079120da227432b
msk226 <kminseok5167@gmail.com>
2025-04-28 19:13:30 +0900

    [FEATURE] 식당 상세 조회 시 프론트 화면 구성용 상세 필드 추가

--- src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/restaurant/RestaurantDetailResponseDTO.java
+++ src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/restaurant/RestaurantDetailResponseDTO.java
@@ -3,11 +3,13 @@ package hyu.erica.capstone.web.dto.tripPlan.response.restaurant;
 import hyu.erica.capstone.domain.Restaurant;
 
 public record RestaurantDetailResponseDTO (
-        Long restaurantId, String name, String imageUrl, String address, String phone, String description, String usageDay){
+        Long restaurantId, String name, String imageUrl, String address, String phone, String description, String usageDay,
+        Double latitude, Double longitude) {
 
     public static RestaurantDetailResponseDTO of(Restaurant restaurant) {
         return new RestaurantDetailResponseDTO(restaurant.getId(),
                 restaurant.getRestaurantName(), "추후 작업 예정", restaurant.getRoadAddress(),
-                restaurant.getPhoneNumber(), restaurant.getDescription(), restaurant.getBusinessHours());
+                restaurant.getPhoneNumber(), restaurant.getDescription(), restaurant.getBusinessHours(),
+                restaurant.getLatitude(), restaurant.getLongitude());
     }
 }
