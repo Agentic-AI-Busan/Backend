eb0f8f1c88e20fbb2f09885d60c778dcf1a27f13
msk226 <kminseok5167@gmail.com>
2025-05-10 15:21:53 +0900

    [FEATURE] 선택지 조회 필드 operatingHour -> operatingHours로 수정

--- src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionDetailResponseDTO.java
+++ src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionDetailResponseDTO.java
@@ -3,7 +3,7 @@ package hyu.erica.capstone.web.dto.tripPlan.response.attraction;
 import hyu.erica.capstone.domain.Attraction;
 
 public record AttractionDetailResponseDTO (Long attractionId, String name, String imageUrl, String address,
-                                           String phone, String title, String operatingHour,
+                                           String phone, String title, String operatingHours,
                                            Double latitude, Double longitude) {
 
     public static AttractionDetailResponseDTO of(Attraction attraction) {
