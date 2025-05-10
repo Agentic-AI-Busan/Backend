9346f9c9db771de75a8a95f74c0c7bd9207d22e2
msk226 <kminseok5167@gmail.com>
2025-05-10 14:47:40 +0900

    [FEATURE] 여행지 영업 시간 필드 명 usageDay -> operatingHour로 통일

--- src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionDetailResponseDTO.java
+++ src/main/java/hyu/erica/capstone/web/dto/tripPlan/response/attraction/AttractionDetailResponseDTO.java
@@ -3,7 +3,7 @@ package hyu.erica.capstone.web.dto.tripPlan.response.attraction;
 import hyu.erica.capstone.domain.Attraction;
 
 public record AttractionDetailResponseDTO (Long attractionId, String name, String imageUrl, String address,
-                                           String phone, String description, String usageDay,
+                                           String phone, String description, String operatingHour,
                                            Double latitude, Double longitude) {
 
     public static AttractionDetailResponseDTO of(Attraction attraction) {
