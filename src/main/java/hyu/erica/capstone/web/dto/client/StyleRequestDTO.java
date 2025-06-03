41f0bedfe1d44e28b3bc89e3c8a6279704048ead
msk226 <kminseok5167@gmail.com>
2025-06-03 21:40:02 +0900

    [FEATURE] 여행 스타일 순서 변경

--- src/main/java/hyu/erica/capstone/web/dto/client/StyleRequestDTO.java
+++ src/main/java/hyu/erica/capstone/web/dto/client/StyleRequestDTO.java
@@ -7,7 +7,7 @@ public record StyleRequestDTO (String city, LocalDate startDate, LocalDate endDa
                                String ageRange, String numberOfPeople, String transportation){
 
     public static StyleRequestDTO of(String city, LocalDate startDate, LocalDate endDate,
-            String preferActivity, String requirement, String preferFood, String dislikedFood,
+            String preferActivity, String preferFood, String dislikedFood, String requirement,
             String ageRange, String numberOfPeople, String transportation) {
         return new StyleRequestDTO(city, startDate, endDate, preferActivity,
                 preferFood, dislikedFood, requirement,
